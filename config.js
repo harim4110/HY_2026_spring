638b125e539edd675bb7d6d2fa1389b9d53103e1
harim4110 <60038150+harim4110@users.noreply.github.com>
2026-03-16 00:17:32 +0900

    Add Apps Script URL

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-export const APPS_SCRIPT_URL = "";
+export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzP6NOQdIiaR78eD54gJgb1KwPAHwl5S8uykXCq0MGgj57-sJ6PiLHsldmYnbdPo4x8zw/exec";
